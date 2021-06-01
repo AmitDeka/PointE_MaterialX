46e1e4a717df03209375e2f75dfcb0869eb02544
AmitDeka <amitdeka49@gmail.com>
2021-06-01 23:26:02 +0530

    Update V1.6.01.2325
    * Themed

--- app/src/main/java/com/android/pointematerialx/SettingPrefActivity.java
+++ app/src/main/java/com/android/pointematerialx/SettingPrefActivity.java
@@ -9,11 +9,16 @@ import androidx.preference.PreferenceManager;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingPrefActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.SummaryProvider<androidx.preference.ListPreference> {
 
+    private Button btnLogout;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,15 @@ public class SettingPrefActivity extends AppCompatActivity implements SharedPref
 
         MaterialToolbar materialToolbar = findViewById(R.id.note_toolbar);
         setSupportActionBar(materialToolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnLogout = findViewById(R.id.btn_logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
 
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
 
